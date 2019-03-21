9f1f00d29b3b8d938e6b29ac94b55ddb891071a7
marko <marko@tensemind.com>
2019-03-21 20:26:38 +0200

    Accumulated amount of simple interest

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,20 @@ pub fn interest(principal: f64, rate: f64, _time: i32) -> f64 {
     return (principal * rate/100.0 * _time as f64) as f64;
 }
 
+pub fn acc_amount(principal: f64, rate: f64, _time: f64) -> f64 {
+    return principal * (1.0 + rate/100.0 * _time);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn test_interest() {
         assert_eq!(interest(2000.0, 5.0, 3), 300.0);
+    }
+    
+    #[test]
+    fn test_acc_amount() {
+        assert_eq!(acc_amount(2000.0, 5.0, 3.0), 2300.0);
     }
 }
