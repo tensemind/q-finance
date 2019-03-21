807ef542e5adc817ff14ef0d00c79180df3ba340
marko <marko@tensemind.com>
2019-03-21 21:11:54 +0200

    Use float everywhere

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-pub fn interest(principal: f64, rate: f64, _time: i32) -> f64 {
-    return (principal * rate/100.0 * _time as f64) as f64;
+pub fn interest(principal: f64, rate: f64, _time: f64) -> f64 {
+    return principal * rate / 100. * _time;
 }
 
 pub fn acc_amount(principal: f64, rate: f64, _time: f64) -> f64 {
-    return principal * (1.0 + rate/100.0 * _time);
+    return principal * (1.0 + rate / 100.0 * _time);
 }
 
 #[cfg(test)]
@@ -11,11 +11,11 @@ mod tests {
     use super::*;
     #[test]
     fn test_interest() {
-        assert_eq!(interest(2000.0, 5.0, 3), 300.0);
+        assert_eq!(interest(2000., 5., 3.), 300.);
     }
     
     #[test]
     fn test_acc_amount() {
-        assert_eq!(acc_amount(2000.0, 5.0, 3.0), 2300.0);
+        assert_eq!(acc_amount(2000., 5.5, 3.), 2330.0);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ extern crate qfinance;
 use qfinance::interest;
 
 fn main() {
-    println!("{}" , interest(2000.0, 5.0, 3));
+    println!("{}" , interest(2000.0, 5.0, 3.0));
 }
