dd026a1f2f8c25c358c3a2ce3eedc694943681d8
marko <marko@tensemind.com>
2019-03-23 18:07:49 +0200

    compound/continuous interest

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
-pub fn interest(principal: f64, rate: f64, _time: f64) -> f64 {
-    return principal * rate / 100. * _time;
+use std::f64::consts::E; 
+
+pub fn simple_interest(principal: f64, rate: f64, time: f64) -> f64 {
+    return principal * rate / 100. * time;
 }
 
-pub fn acc_amount(principal: f64, rate: f64, _time: f64) -> f64 {
-    return principal * (1.0 + rate / 100.0 * _time);
+pub fn acc_amount(principal: f64, rate: f64, time: f64) -> f64 {
+    return principal * (1.0 + rate / 100.0 * time);
 }
 
 pub fn cash_flow(income: f64, expenses: f64) -> f64 {
@@ -14,13 +16,21 @@ pub fn rule_72(rate: f64) -> f64 {
     return 72. / rate;
 }
 
+pub fn compound_interest(principal: f64, rate: f64, time: f64, conversions: f64) -> f64 {
+    return principal * (1.0 + rate / 100.0 / conversions).powf(time * conversions);
+}
+
+pub fn continuous_interest(principal: f64, rate: f64, time: f64) -> f64 {
+    return principal * (E.powf(rate / 100.0 * time));
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_interest() {
-        assert_eq!(interest(2000., 5., 3.), 300.);
+    fn test_simple_interest() {
+        assert_eq!(simple_interest(2000., 5., 3.), 300.);
     }
     
     #[test]
@@ -36,5 +46,16 @@ mod tests {
     #[test]
     fn test_rule_72() {
         assert_eq!(rule_72(7.5), 9.6);
+    }
+    
+    #[test]
+    fn test_compound_interest() {
+        assert_eq!(format!("{:.2}", compound_interest(2000., 5., 3., 12.)), "2322.94");
+        assert_eq!(format!("{:.2}", compound_interest(2000., 5., 3., 365.)), "2323.64");
+    }
+    
+    #[test]
+    fn test_continuous_interest() {
+        assert_eq!(format!("{:.2}", continuous_interest(2000., 5., 3.)), "2323.67");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate qfinance;
 
-use qfinance::interest;
+use qfinance::{simple_interest, compound_interest, continuous_interest};
 
 fn main() {
-    println!("{}" , interest(2000.0, 5.0, 3.0));
+    println!("{}" , simple_interest(2000.0, 5.0, 3.0));
+    println!("{}" , format!("{:.2}", compound_interest(2000., 5., 3., 365.)));
+    println!("{}" , format!("{:.2}", continuous_interest(2000., 5., 3.)));
+    
 }
